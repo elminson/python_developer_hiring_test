e60fb9c3fb6b6faac77a1dffc0dec557a8373401
hmtanbir <tanbir2025@gmail.com>
2018-04-25 20:18:33 +0600

    q16 added

---
+++ q16.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import math
+
+
+class Data(object):
+    def __init__(self, name):
+        self.__name = name
+        self.__links = set()
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def links(self):
+        return set(self.__links)
+
+    def add_link(self, other):
+        self.__links.add(other)
+        other.__links.add(self)
+
+        # The function to look for connected components.
+
+
+def connected_components(nodes):
+    # List of connected components found. The order is random.
+    result = []
+
+    # Make a copy of the set, so we can modify it.
+    nodes = set(nodes)
+
+    # Iterate while we still have nodes to process.
+    while nodes:
+
+        # Get a random node and remove it from the global set.
+        n = nodes.pop()
+
+        # This set will contain the next group of nodes connected to each other.
+        group = {n}
+
+        # Build a queue with this node in it.
+        queue = [n]
+
+        # Iterate the queue.
+        # When it's empty, we finished visiting a group of connected nodes.
+        while queue:
+            # Consume the next item from the queue.
+            n = queue.pop(0)
+
+            # Fetch the neighbors.
+            neighbors = n.links
+
+            # Remove the neighbors we already visited.
+            neighbors.difference_update(group)
+
+            # Remove the remaining nodes from the global set.
+            nodes.difference_update(neighbors)
+
+            # Add them to the group of connected nodes.
+            group.update(neighbors)
+
+            # Add them to the queue, so we visit them in the next iterations.
+            queue.extend(neighbors)
+
+        # Add the group to the list of groups.
+        result.append(group)
+
+    # Return the list of groups.
+    return result
+
+    # The test code...
+
+
+def minimalCost(n, pairs):
+    nodes_map = {x: Data(x) for x in xrange(1, n + 1)}
+
+    for pair in pairs:
+        p, q = int(pair.split(' ')[0]), int(pair.split(' ')[1])
+        nodes_map[p].add_link(nodes_map[q])
+
+    # Find all the connected components.
+    cost = 0
+    for components in connected_components(nodes_map.values()):
+        # names = sorted(node.name for node in components)
+        # print names
+        cost += math.ceil(math.sqrt(len(components)))
+    return long(cost)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    pairs = list()
+    for _ in range(input()):
+        pairs.append(raw_input())
+    print minimalCost(n, pairs)
