cdb38cb5997f071d7113b7705e57ab8eb00e2b0b
hmtanbir <tanbir2025@gmail.com>
2018-04-25 20:06:52 +0600

    :dog:

--- q6.py
+++ q6.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+
 def calculateMaximumProfit(cost_per_cut, metal_price, lengths):
     maxProfit = 0
     curProfit = 0
@@ -7,11 +8,11 @@ def calculateMaximumProfit(cost_per_cut, metal_price, lengths):
     totalRods = 0
     totalCuts = 0
 
-#  Find out maximum length
+    #  Find out maximum length
     for curLength in lengths:
-            maxLength = max(maxLength, curLength)
+        maxLength = max(maxLength, curLength)
 
-#  For each of the possible rod lengths, calculate possible profit
+    #  For each of the possible rod lengths, calculate possible profit
     curLength = 1
     while curLength < maxLength:
         totalRods = 0
@@ -19,8 +20,8 @@ def calculateMaximumProfit(cost_per_cut, metal_price, lengths):
         #  Cut each of the rods into smaller rods of size curLength
         #  Count total rods and total cuts
         for length in lengths:
-                totalRods += (length / curLength)
-                totalCuts += ((length - 1) / curLength)
+            totalRods += (length / curLength)
+            totalCuts += ((length - 1) / curLength)
         #  Calculate current profit
         curProfit = totalRods * curLength * metal_price - totalCuts * cost_per_cut
         #  Calculate maximum profit
@@ -29,16 +30,16 @@ def calculateMaximumProfit(cost_per_cut, metal_price, lengths):
     # prints maximum profit
     print maxProfit
 
+
 if __name__ == '__main__':
     cost_per_cut = int(input())
     metal_price = int(input())
     # array input of metals length
     lengths_array = list()
     lengths_count = int(input())
-    
+
     for i in range(int(lengths_count)):
         n = input()
-        lengths_array.append(int(n))   
-    # print out the result    
+        lengths_array.append(int(n))
+        # print out the result
     calculateMaximumProfit(cost_per_cut, metal_price, lengths_array)
-    
